3d094107864c601302e4cd042827cce6d00a7909
TJ202 <72973488+TJ202@users.noreply.github.com>
2021-11-13 20:14:35 +0530

    info added

--- js/main copy.js
+++ js/main copy.js
@@ -41,34 +41,43 @@ let t1 = `
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/kadma.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b> Kadamat has been identified for staying tourists with its tourist
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b> Kadamat has been identified for staying tourists with its tourist
 aesthetically situated in the coconut palm groves in the beaches facing
 the lagoon. <br>
 <b>Location : </b>11°-13* North Latitude 72°-47* East longitude <br>
-<b>Land Area : </b>3.12 sq km  </p>
+<b>Land Area : </b>3.12 sq km <br> 
+</p><p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
+</p>
 `
 let t2 = `
 <h3 style="margin-left:0">Bitra</h3>
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/bitra.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>It is the smallest inhabited island in the territory. There is a small
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>It is the smallest inhabited island in the territory. There is a small
 shrine of an old Arab Saint which is a place of pilgrimage for the people
 of other islands. <br>
 <b>Location : </b>11°- 36* North Latitude 72°- 10* East longitude
 <br>
 <b>Land Area : </b>0.105 sq km </p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
+</p>
 `
 let t3 = `
 <h3 style="margin-left:0">Chetlat</h3>
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/chetlat.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b> A quiet place for long walks on pristine shorelines swaying by
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b> A quiet place for long walks on pristine shorelines swaying by
 the coconut tree gardens under the oblique sun while surrounded by the
 gleaming turquoise ocean.<br>
 <b>Location : </b> 11°- 41* North Latitude 72°- 43* East longitude <br>
 <b>Land Area : </b>1.4 sq km
+</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
 </p>
 `
 let t4 = `
@@ -76,11 +85,14 @@ let t4 = `
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/kilthan.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>Attractive by its thick flora, the summer nights in kilthan are
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>Attractive by its thick flora, the summer nights in kilthan are
 warmer and it is usual for the people to sleep outside on the beaches.
 This place is known for its rich tradition of folk dances. <br>
 <b>Location : </b>11°- 29* North Latitude 73°- 04* East longitude <br>
 <b>Land Area : </b>2.2 sq km
+</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
 </p>
 `
 let t5 = `
@@ -88,10 +100,13 @@ let t5 = `
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/tinnakara.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>The panorama encompasses magnificent lagoons, sun drenched
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>The panorama encompasses magnificent lagoons, sun drenched
 sand and spectacular marine flora and fauna.<br>
 <b>Location : </b>10.9471° N, 72.3185° E<br>
 <b>Land Area : </b>2.3 sq km
+</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
 </p>
 `
 let t6 = `
@@ -99,32 +114,39 @@ let t6 = `
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/bangaram.png"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>A bewitchingly beautiful and breath-taking island in
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>A bewitchingly beautiful and breath-taking island in
 Lakshadweep. Sparkling coral reef and blue lagoon perform magic on the
 soul searching travellers.<br>
 <b>Location : </b> 10°- 56* North Latitude and 72°- 17* East longitude<br>
 <b>Land Area : </b>2.3 sq km</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
+</p>
 `
 let t7 = `
 <h3 style="margin-left:0">Amini</h3>
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/amini.png"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b> It is known for its talented craftsmen for making walking sticks
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b> It is known for its talented craftsmen for making walking sticks
 with tortoise shells and coconut shells and has a rich tradition of folk
 songs.<br>
 <b>Location : </b>11°- 07* North Latitude 72°- 44* East longitude<br>
 <b>Land Area : </b>2.6 sq km</p>
+
 `
 let t8 = `
 <h3 style="margin-left:0">Agatti</h3>
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/agatti.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>It is one of the most beautiful lagoons in Lakshadweep with the
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>It is one of the most beautiful lagoons in Lakshadweep with the
 airport built on this island.<br>
 <b>Location : </b>10°- 51* North Latitude 72° longitude<br>
 <b>Land Area : </b>3.84 sq km
+</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
 </p>
 `
 let t9 = `
@@ -132,30 +154,39 @@ let t9 = `
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/kavaratti.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b> The capital of Lakshadweep island, known for its sand beaches and calm lagoons.<br>
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b> The capital of Lakshadweep island, known for its sand beaches and calm lagoons.<br>
 <b>Location : </b> 10° 32’ and 10° 35’ North latitude and 72° 35’ and 72° 40’ East
 longitude<br>
 <b>Land Area : </b>4.22 sq km</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
+</p>
 `
 let t10 = `
 <h3 style="margin-left:0">Suheli</h3>
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/suheli.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>It has a breathtaking view with sparkling coral reef, lush green
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>It has a breathtaking view with sparkling coral reef, lush green
 coconut palms and silvery beaches.<br>
 <b>Location : </b>10° 05’ North latitude and 72° 17’ East longitude<br>
 <b>Land Area : </b>0.88 sq km </p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
+</p>
 `
 let t11 = `
 <h3 style="margin-left:0">Kalpeni</h3>
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/kalpeni.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>One can swim, snorkel or reef-walk in the vast lagoon and
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>One can swim, snorkel or reef-walk in the vast lagoon and
 indulge in water sports on kayaks, sailboats and pedal boats.<br>
 <b>Location : </b>10°- 05* North Latitude and 73°- 39* East latitude<br>
 <b>Land Area : </b>3.08 sq km
+</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
 </p>
 `
 let t12 = `
@@ -163,22 +194,28 @@ let t12 = `
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/andrott.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>It is the largest island in Lakshadweep. The tomb of Hazrat
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>It is the largest island in Lakshadweep. The tomb of Hazrat
 Ubaidullah and the Buddhist archaeological remains to be the attraction
 of the island.<br>
 <b>Location : </b>10°- 49* North Latitude 73°- 41* East longitude<br>
 <b>Land Area : </b>4.9 sq km</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
+</p>
 `
 let t13 = `
 <h3 style="margin-left:0">Minicoy</h3>
 <div style="text-align:center">
     <img style="max-width:100%; width:100%" src="./assets/mapim/minicoy.jpg"/>
 </div> 
-<p style="margin-top:10px"><b>Glance : </b>The light house of the island is the main attraction along with
+<p style="margin-top:10px; margin-bottom:0;"><b>Glance : </b>The light house of the island is the main attraction along with
 long drives through dense coconut groves and winding village roads. It’s
 a place to swim, beach walk, pedal boat, kayak and sail.<br>
 <b>Location : </b>8°- 17* North Latitude 73°- 04* East longitude<br>
 <b>Land Area : </b>4.8 sq km</p>
+<p style="text-align: end; margin-top:0; color:black;">
+<a href="./html/activities.html#focal"><b style="color:black;text-decoration: underline;">Know More ></b></a> 
+</p>
 `
 
 //popup decl
